15e5e38dc868644b44ac8f90332b15221235d2bd
OkanoKensin <ttc22c1020012@edu.tech.ac.jp>
2022-06-23 15:45:00 +0900

    Update playCharacter.cs

--- Fitcine/Assets/Script/playCharacter.cs
+++ Fitcine/Assets/Script/playCharacter.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playCharacter : MonoBehaviour
+public class PlayerJumpController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rbody2D;
+
+    private float jumpForce = 350f;
+
+    private int jumpCount = 0;
+
     void Start()
     {
-        
+        rbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftShift) && this.jumpCount < 1)
+        {
+            this.rbody2D.AddForce(transform.up * jumpForce);
+            jumpCount++;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            jumpCount = 0;
+        }
     }
 }
